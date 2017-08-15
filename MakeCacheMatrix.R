292fb2916d8a0e70944d4a8aa426741c25476e54
Srishtijain0891 <30552844+Srishtijain0891@users.noreply.github.com>
2017-08-15 17:00:53 +0530

    Update MakeCacheMatrix.R

--- MakeCacheMatrix.R
+++ MakeCacheMatrix.R
@@ -40,37 +40,3 @@ cacheSolve <- function(x, ...) {
   
   inv
 }
-
-# Test script for MakeCacheMatrix.R
-
-# Approach 1: create a matrix object, then use it as input to cacheSolve()
-
-a <- makeCacheMatrix(matrix(c(-1, -2, 1, 1), 2,2))
-cacheSolve(a)
-
-# Call cacheSolve(a) a second time to trigger the "Getting cached matrix inverse" message
-cacheSolve(a)
-
-# Multiply the matrix by inverse, resulting in identity matrix
-a$get() %*% a$getinverse()
-
-# Reset a with another matrix to clear out cached value
-a$set(matrix(c(2,3,2,2),2,2))
-
-# Confirm that a has new data and that cache is NULL
-a$get()
-
-a$getinverse()
-
-# Rerun cache solve, note that "Getting cached matrix inverse" does not print,
-# and that we get a different result
-cacheSolve(a)
-
-# Approach 2: use makeCacheMatrix() as the input argument to cacheSolve()
-#             note that the argument to cacheSolve() is a different object
-#             than the argument to the first call of cacheSolve()
-cacheSolve(makeCacheMatrix(matrix(c(-1, -2, 1, 1), 2,2)))
-
-# Try a non-invertible matrix
-b <- makeCacheMatrix(matrix(c(0,0,0,0),2,2))
-cacheSolve(b)
